f05f36f826050719f01385c6b058345d53a6bf69
Pedro Haka <pedrohaka94@gmail.com>
2024-04-18 13:46:32 -0300

    First commit

---
+++ api/routes/orders.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'You have fetched the orders list successfully!'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'You have successfully created a new order!'
+    })
+})
+
+router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId
+
+    res.status(200).json({
+        message: `You have successfully fetched the order with ID: ${id}!`,
+        id: id
+    })
+})
+
+router.delete('/:orderId', (req, res, next) => {
+    const id = req.params.orderId
+
+    res.status(200).json({
+        message: `You have successfully deleted the order with ID: ${id}!`,
+        id: id
+    })
+})
+
+module.exports = router
---
+++ api/routes/products.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /products'
+    })
+})
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId
+
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'You have passed the "special" ID',
+            id: id
+        })
+    }
+    else{
+        res.status(200).json({
+            message: 'You have passed a random ID',
+            id: id
+        })
+    }
+})
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    
+    res.status(200).json({
+        message: `Updated the product with ID:${id}`,
+        id: id
+    })    
+})
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    
+    res.status(200).json({
+        message: `Deleted the product with ID:${id}`,
+        id: id
+    }) 
+})
+   
+
+module.exports = router
