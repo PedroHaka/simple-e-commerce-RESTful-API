3b1268e87c4fa6f43e0517c7ca90eac231004c13
Pedro Haka <pedrohaka94@gmail.com>
2024-04-19 20:47:26 -0300

    Add Body Parser - Test POST Body Requests with Postman

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
+const morgan = require('morgan')//morgan is a log service,
+//everytime a request is made to our server, it logs out in
+//the terminal what kind of request was made.
+const bodyParser = require('body-parser')//body parser is 
+//a tool that lets us parse our request bodies, as it is
+//not easily readable by default on node.js. 
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
