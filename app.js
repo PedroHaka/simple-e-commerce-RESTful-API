9e195bfd01fce7f5f0bde266d476ee910d02844d
Pedro Haka <pedrohaka94@gmail.com>
2024-04-19 21:10:18 -0300

    Handle CORS issues by defining request/response headers

--- app.js
+++ app.js
@@ -7,11 +7,26 @@ const bodyParser = require('body-parser')//body parser is
 //a tool that lets us parse our request bodies, as it is
 //not easily readable by default on node.js. 
 
+
+//Here we deal with CORS issues by sending the apropriate headers on our requests/responses.
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control_Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    if (req.method === "OPTIONS") {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+        return res.status(200).json({})
+    }
+    next()
+})
+
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/products', productRoutes)
